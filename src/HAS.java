7abdf972426386af16cb4c4daa853917152ad8aa
akshay5akki@gmail.com <admin-PC@DESKTOP-VRSJBU8>
2017-06-07 23:03:12 -0700

    Message 2

--- src/HAS.java
+++ src/HAS.java
@@ -1,4 +1,19 @@
-
-public class HAS {
-
+public class HAS 
+{
+ private String name;
+ private int no,size;
+ 
+ public HAS(String name, int no,int size)
+ {
+	 this.name=name;
+	 this.no=no;
+	 this.size=size;
+ }
+ 
+ public String getName()
+ {
+	 
+ }
+ 
+ 
 }
